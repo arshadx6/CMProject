2c1dc668fe9ace663e08313835dc3adb10bb2fbd
arshadx6 <73222358+arshadx6@users.noreply.github.com>
2022-12-21 07:26:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,7 @@ if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
 col1, col2= st.columns(2)
-col1.header("hello")
+ad = [ab,cd]
+
+col1.header(ad)
 col2.metric("Humidity", "86%", "4%")
