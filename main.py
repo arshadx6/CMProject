77982cafce1736916374d32605964a1e684f1425
arshadx6 <73222358+arshadx6@users.noreply.github.com>
2022-12-21 06:55:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,5 @@ if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
+
 col3.metric("Humidity", "86%", "4%")
