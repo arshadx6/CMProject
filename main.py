863332a61c89522b244a9ffbdec1e896e697c29e
arshadx6 <73222358+arshadx6@users.noreply.github.com>
2022-12-21 06:46:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import streamlit as st
+from PIL import Image
 st.write("Hello from Streamlitssss")
+st.title(" Welcome to the smart blood report generator ")
+image = Image.open('sunrise.svg')
+st.image(image, caption='Sunrise by the mountains')
