dd0087ffd4eb54b39e0125b48e39a4325de730d4
arshadx6 <73222358+arshadx6@users.noreply.github.com>
2022-12-21 09:12:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ for i in df.values:
 
     with col2:
         st.caption(i[6])
-        st.caption(str(i[4]+i[3]))
+        st.caption(str(i[4])+str(i[3]))
         if(i[5]=="Normal"):
             st.success(i[6])
         elif(i[5]=="High"):
