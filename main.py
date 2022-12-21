f901b63a8c62234d9c5d0a81c6ac424b85c6951b
arshadx6 <73222358+arshadx6@users.noreply.github.com>
2022-12-21 08:24:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,14 +39,14 @@ st.markdown("""
 for i in range(3):  
     col1, col2 , col3 = st.columns(3)
     with col1:
-   st.header(mydata[i][i])
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+        st.header(mydata[i][i])
+        st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col2:
-   st.metric("Humidity", "86%", "-8%")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+    with col2:
+        st.metric("Humidity", "86%", "-8%")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
 
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+    with col3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
 
